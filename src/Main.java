94cf544bec2dccc40ded88a8d4424faf9652f925
LucyMkr <lucymkrtchyan52@gmail.com>
2022-12-24 02:13:20 +0400

    added new homework

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+        int c = a*a + b*b;
+        System.out.println(c);
+        //..............................................................................................................
+         a = 10;
+         b = 5;
+         c = 8;
+        System.out.println((a+b)*c/2);
+        //..............................................................................................................
+        int x = 234;
+        int y = x/100;
+        int z = ((x-y*100)/10);
+        int m = ((x-y*100-z*10));
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(m);
+
+
+
+
+    }
+}
