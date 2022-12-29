5f489195a0c8ace91921a872606eee1e77ae0fe3
LucyMkr <lucymkrtchyan52@gmail.com>
2022-12-29 23:14:26 +0400

    added third homework

---
+++ src/secondmain.java
@@ -0,0 +1,35 @@
+
+
+public class secondmain {
+    public static void main(String[] args) {
+
+        int x = 56;
+        int z = 168;
+        float p1;
+        p1 = (float) z/x;
+        boolean b1 = true;
+        boolean b2 = false;
+        if (z/x == p1) {
+            System.out.println(b1);
+        }
+        else if (z/x !=p1) {
+            System.out.println(b2);
+        }
+        //.............................................................................................................
+        int a = 7;
+        int b = 10;
+        int c = 48;
+        int d = 134;
+        int e = 23;
+        int f = 49;
+        float k = (a+b+c)/3F;
+        System.out.println(k);
+        float m = (d+e+f)/3F;
+        System.out.println(m);
+        int i = (int) ((k+m)/2);
+        System.out.println(i);
+    }
+}
+
+
+
