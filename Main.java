0bda4c443e0ed97176dddeae4a5cb66ed75d0321
LucyMkr <lucymkrtchyan52@gmail.com>
2022-12-31 11:21:18 +0400

    added new homework

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double d = 23.4567;
+        int i = (int) d;
+        double d1 = d - i;
+        System.out.println(d1);
+    }
+}
