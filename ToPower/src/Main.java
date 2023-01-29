688e362be9c05ad39bea7cf7f622d775491e9991
LucyMkr <lucymkrtchyan52@gmail.com>
2023-01-29 19:41:23 +0400

    homework on methods (power number)

---
+++ ToPower/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        toThePower(2, 7);
+        toThePower(11, 2);
+        toThePower(3, 13);
+        toThePower(-1, 9);
+    }
+
+    public static int toThePower(int base, int exponent) {
+        if (base > 0 && base < 10 && (exponent > 0 && exponent < 10 || exponent < 0 && exponent > -10) ) {
+            int power = (int) Math.pow(base, exponent);
+            System.out.println(power);
+        }
+        else if (base > 10 || exponent > 10 ) {
+            System.out.println("-1");
+            System.out.println("Wrong base or exponent");
+            System.out.println("Please, input correct data");
+
+        }
+        else if (base <= 0) {
+            System.out.println("-2");
+            System.out.println("Wrong base");
+            System.out.println("Please, input correct data");
+        }
+        return base;
+    }
+}
